295627d0552440fe44ca899a827c242f0ece0c65
Piotr Samborski <piotr.samborski@onedynamics.pl>
2022-01-16 21:01:16 +0100

    bezier cube

--- Geometric2/ModelGeneration/Cube.cs
+++ Geometric2/ModelGeneration/Cube.cs
@@ -32,9 +32,6 @@ namespace Geometric2.ModelGeneration
             //GeneratePatches(first_globalPhysicsData);
             cubePoints = new float[] { };
             indices = new uint[] { };
-
-            //cubePoints = new float[ConfigurationData.BezierBoxDivisions * ConfigurationData.BezierBoxDivisions * 8];
-            //indices = new uint[] { 0, 1, 2 };
             _shaderLight.Use();
             texture = new Texture("./../../../Resources/wood.jpg");
             specular = new Texture("./../../../Resources/50specular.png");
@@ -44,9 +41,9 @@ namespace Geometric2.ModelGeneration
             cubeEBO = GL.GenBuffer();
             GL.BindVertexArray(cubeVAO);
             GL.BindBuffer(BufferTarget.ArrayBuffer, cubeVBO);
-            GL.BufferData(BufferTarget.ArrayBuffer, cubePoints.Length * sizeof(float), cubePoints, BufferUsageHint.DynamicDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, cubePoints.Length * sizeof(float), cubePoints, BufferUsageHint.StaticDraw);
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, cubeEBO);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.DynamicDraw);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
             var a_Position_Location = _shaderLight.GetAttribLocation("a_Position");
             GL.VertexAttribPointer(a_Position_Location, 3, VertexAttribPointerType.Float, true, 8 * sizeof(float), 0);
             GL.EnableVertexAttribArray(a_Position_Location);
@@ -63,10 +60,6 @@ namespace Geometric2.ModelGeneration
             if (globalPhysicsData.displayBezierCube)
             {
                 GeneratePatches(globalPhysicsData);
-                //cubePoints = new float[] { -1.5f,-1.5f,-1.5f, -1,0,0,0.5f,0.5f,
-                //-1.5f,1.5f,-1.5f, -1,0,0,0.5f,0.5f,
-                //-1.5f,-1.5f,1.5f, -1,0,0,0.5f,0.5f};
-                //indices = new uint[] { 0, 1, 2 };
                 RecalculateGeometry();
                 _shaderLight.Use();
                 Matrix4 model = ModelMatrix.CreateModelMatrix(new Vector3(1.0f, 1.0f, 1.0f), RotationQuaternion, CenterPosition + Translation, rotationCentre, TempRotationQuaternion);
@@ -85,45 +78,37 @@ namespace Geometric2.ModelGeneration
         {
             GL.BindVertexArray(cubeVAO);
             GL.BindBuffer(BufferTarget.ArrayBuffer, cubeVBO);
-            GL.BufferData(BufferTarget.ArrayBuffer, cubePoints.Length * sizeof(float), cubePoints, BufferUsageHint.DynamicDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, cubePoints.Length * sizeof(float), cubePoints, BufferUsageHint.StaticDraw);
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, cubeEBO);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.DynamicDraw);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
         }
 
         private void GeneratePatches(GlobalPhysicsData globalPhysicsData)
         {
             List<uint> indicesList = new List<uint>();
-            List<Vector3> points = new List<Vector3>();
             var commonIndices = GenerateIndices();
             uint numberOfIndicesInPatch = (uint)commonIndices.Length;
             uint maxNoIndice = (uint)(commonIndices.Max());
             var patches = GetC0Patches(globalPhysicsData);
             uint idx = 0;
+            List<float> tempCubePoints = new List<float>();
             foreach (var patch in patches)
             {
                 Vector3[] patchPoints = GeneratePatchPoints(patch.Item1);
-                points.AddRange(patchPoints);
+                tempCubePoints.AddRange(GenerateOnlyPoints(patchPoints.ToList(), patch.Item2));
                 for (int i = 0; i < numberOfIndicesInPatch; i++)
                 {
-                    indicesList.Add(commonIndices[i] + idx * maxNoIndice + 1);
+                    indicesList.Add(commonIndices[i] + idx * maxNoIndice + idx);
                 }
 
                 idx++;
             }
 
-            //if (indices == null)
-            {
-                indices = indicesList.ToArray();
-            }
-
-            var tempCubePoints = GenerateOnlyPoints(points);
-            //if (cubePoints == null)
-            {
-                cubePoints = tempCubePoints;
-            }
+            indices = indicesList.ToArray();
+            cubePoints = tempCubePoints.ToArray();
         }
 
-        private float[] GenerateOnlyPoints(List<Vector3> points)
+        private float[] GenerateOnlyPoints(List<Vector3> points, int numer)
         {
             var tempCubePoints = new float[8 * points.Count];
             int idx = 0;
@@ -133,9 +118,42 @@ namespace Geometric2.ModelGeneration
                 tempCubePoints[8 * idx + 1] = p.Y;
                 tempCubePoints[8 * idx + 2] = p.Z;
 
-                tempCubePoints[8 * idx + 3] = -1;
-                tempCubePoints[8 * idx + 4] = 0;
-                tempCubePoints[8 * idx + 5] = 0;
+                switch (numer)
+                {
+                    case 0:
+                        tempCubePoints[8 * idx + 3] = -1;
+                        tempCubePoints[8 * idx + 4] = 0;
+                        tempCubePoints[8 * idx + 5] = 0;
+                        break;
+
+                    case 1:
+                        tempCubePoints[8 * idx + 3] = 1;
+                        tempCubePoints[8 * idx + 4] = 0;
+                        tempCubePoints[8 * idx + 5] = 0;
+                        break;
+
+                    case 2:
+                        tempCubePoints[8 * idx + 3] = 0;
+                        tempCubePoints[8 * idx + 4] = -1;
+                        tempCubePoints[8 * idx + 5] = 0;
+                        break;
+                    case 3:
+                        tempCubePoints[8 * idx + 3] = 0;
+                        tempCubePoints[8 * idx + 4] = 1;
+                        tempCubePoints[8 * idx + 5] = 0;
+                        break;
+
+                    case 4:
+                        tempCubePoints[8 * idx + 3] = 0;
+                        tempCubePoints[8 * idx + 4] = 0;
+                        tempCubePoints[8 * idx + 5] = -1;
+                        break;
+                    case 5:
+                        tempCubePoints[8 * idx + 3] = 0;
+                        tempCubePoints[8 * idx + 4] = 0;
+                        tempCubePoints[8 * idx + 5] = 1;
+                        break;
+                }
 
                 tempCubePoints[8 * idx + 6] = 0.5f;
                 tempCubePoints[8 * idx + 7] = 0.5f;
@@ -147,7 +165,7 @@ namespace Geometric2.ModelGeneration
 
         private List<(Vector3[], int)> GetC0Patches(GlobalPhysicsData globalPhysicsData)
         {
-            int wallNo = 2;
+            int wallNo = 6;
             var points = globalPhysicsData.points;
             var pointsIndicesForPatches = new int[wallNo][];
             for (int i = 0; i < wallNo; i++)
@@ -165,20 +183,20 @@ namespace Geometric2.ModelGeneration
                 pointsIndicesForPatches[1][i] = i + 48; //TODO: change to 1
             }
 
-            //pointsIndicesForPatches[2] = new int[] { 0, 1, 2, 3, 16, 17, 18, 19, 32, 33, 34, 35, 48, 49, 50, 51 };
+            pointsIndicesForPatches[2] = new int[] { 0, 1, 2, 3, 16, 17, 18, 19, 32, 33, 34, 35, 48, 49, 50, 51 };
 
-            //for (int i = 0; i < 16; i++)
-            //{
-            //    pointsIndicesForPatches[3][i] = pointsIndicesForPatches[2][i] + 12;
-            //}
+            for (int i = 0; i < 16; i++)
+            {
+                pointsIndicesForPatches[3][i] = pointsIndicesForPatches[2][i] + 12;
+            }
 
 
-            //pointsIndicesForPatches[4] = new int[] { 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60 };
+            pointsIndicesForPatches[4] = new int[] { 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60 };
 
-            //for (int i = 0; i < 16; i++)
-            //{
-            //    pointsIndicesForPatches[5][i] = pointsIndicesForPatches[4][i] + 3;
-            //}
+            for (int i = 0; i < 16; i++)
+            {
+                pointsIndicesForPatches[5][i] = pointsIndicesForPatches[4][i] + 3;
+            }
 
             List<(Vector3[], int)> result = new List<(Vector3[], int)>();
             for (int i = 0; i < wallNo; i++)
