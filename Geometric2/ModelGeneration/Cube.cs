0ba481c2bd74c22d8ab1837371a9cff70d5bd15e
Piotr Samborski <piotr.samborski@onedynamics.pl>
2022-01-16 16:27:30 +0100

    add bezier cube not working

--- Geometric2/ModelGeneration/Cube.cs
+++ Geometric2/ModelGeneration/Cube.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using Geometric2.Global;
 using Geometric2.Helpers;
@@ -62,6 +63,7 @@ namespace Geometric2.ModelGeneration
         Texture texture;
         Texture specular;
         Texture noise;
+        public GlobalPhysicsData first_globalPhysicsData;
         public CubeData CubeData { get; set; }
 
         public Cube()
@@ -75,7 +77,7 @@ namespace Geometric2.ModelGeneration
 
         public override void CreateGlElement(Shader _shader, Shader _shaderLight)
         {
-            cubePoints = ChangePointAndNormalsPositions(cubePoints);
+            GeneratePatches(first_globalPhysicsData);
             _shaderLight.Use();
             texture = new Texture("./../../../Resources/wood.jpg");
             specular = new Texture("./../../../Resources/50specular.png");
@@ -101,10 +103,12 @@ namespace Geometric2.ModelGeneration
 
         public override void RenderGlElement(Shader _shader, Shader _shaderLight, Vector3 rotationCentre, GlobalPhysicsData globalPhysicsData)
         {
-            //if (globalPhysicsData.displayWalls)
+            if (globalPhysicsData.displayBezierCube)
             {
+                GeneratePatches(globalPhysicsData);
+                RecalculateGeometry();
                 _shaderLight.Use();
-                Matrix4 model = ModelMatrix.CreateModelMatrix(new Vector3(1.0f,1.0f,1.0f), RotationQuaternion, CenterPosition + Translation, rotationCentre, TempRotationQuaternion);
+                Matrix4 model = ModelMatrix.CreateModelMatrix(new Vector3(1.0f, 1.0f, 1.0f), RotationQuaternion, CenterPosition + Translation, rotationCentre, TempRotationQuaternion);
                 _shaderLight.SetMatrix4("model", model);
                 _shaderLight.SetInt("transparent", 1);
                 GL.BindVertexArray(cubeVAO);
@@ -116,33 +120,156 @@ namespace Geometric2.ModelGeneration
             }
         }
 
-        private float[] ChangePointAndNormalsPositions(float[] pointsNormalsTextCoords)
+        private void RecalculateGeometry()
         {
-            float[] result = new float[pointsNormalsTextCoords.Length];
-            for (int i = 0; i < pointsNormalsTextCoords.Length; i += 8)
+            GL.BindVertexArray(cubeVAO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, cubeVBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, cubePoints.Length * sizeof(float), cubePoints, BufferUsageHint.DynamicDraw);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, cubeEBO);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.DynamicDraw);
+        }
+
+        private void GeneratePatches(GlobalPhysicsData globalPhysicsData)
+        {
+            List<uint> indices = new List<uint>();
+            List<Vector3> points = new List<Vector3>();
+            var commonIndices = GenerateIndices();
+            uint numberOfIndicesInPatch = (uint)commonIndices.Length;
+            var patches = GetC0Patches(globalPhysicsData);
+            uint idx = 0;
+            foreach (var patch in patches)
             {
-                var modelMtxPoints = ModelMatrix.CreateModelMatrix(1.0f, (float)Math.PI / 4, 0.0f, (float)Math.Atan(Math.Sqrt(2) / 2), new Vector3(0, (float)Math.Sqrt(3) / 2, 0));
-                var modelMtxNormals = ModelMatrix.CreateModelMatrix(1.0f, (float)Math.PI / 4, 0.0f, (float)Math.Atan(Math.Sqrt(2) / 2), new Vector3(0, 0, 0));
+                Vector3[] patchPoints = GeneratePatchPoints(patch);
+                points.AddRange(patchPoints);
+                uint[] patchIndices = new uint[numberOfIndicesInPatch];
+                for (int i = 0; i < numberOfIndicesInPatch; i++)
+                {
+                    indices.Add(commonIndices[i] + idx * numberOfIndicesInPatch);
+                }
+            }
+            GenerateOnlyPoints(points);
+        }
 
-                Vector4 position = new Vector4(pointsNormalsTextCoords[i], pointsNormalsTextCoords[i + 1], pointsNormalsTextCoords[i + 2], 1.0f);
-                Vector4 normal = new Vector4(pointsNormalsTextCoords[i + 3], pointsNormalsTextCoords[i + 4], pointsNormalsTextCoords[i + 5], 1.0f);
+        private void GenerateOnlyPoints(List<Vector3> points)
+        {
+            cubePoints = new float[3 * points.Count];
+            int idx = 0;
+            foreach (var p in points)
+            {
+                cubePoints[3 * idx] = p.X;
+                cubePoints[3 * idx + 1] = p.Y;
+                cubePoints[3 * idx + 2] = p.Z;
+                idx++;
+            }
+        }
 
-                position = position * modelMtxPoints;
-                normal = normal * modelMtxNormals;
+        private List<Vector3[]> GetC0Patches(GlobalPhysicsData globalPhysicsData)
+        {
+            var points = globalPhysicsData.points;
+            var pointsIndicesForPatches = new int[6][];
+            for(int i = 0; i < 6; i++)
+            {
+                pointsIndicesForPatches[i] = new int[16];
+            }
 
-                result[i] = position.X;
-                result[i + 1] = position.Y;
-                result[i + 2] = position.Z;
+            for (int i = 0; i < 16; i++)
+            {
+                pointsIndicesForPatches[0][i] = i;
+            }
 
-                result[i + 3] = normal.X;
-                result[i + 4] = normal.Y;
-                result[i + 5] = normal.Z;
+            for (int i = 0; i < 16; i++)
+            {
+                pointsIndicesForPatches[1][i] = pointsIndicesForPatches[0][i] + 48;
+            }
 
-                result[i + 6] = pointsNormalsTextCoords[i + 6];
-                result[i + 7] = pointsNormalsTextCoords[i + 7];
+            pointsIndicesForPatches[2] = new int[] { 0,1,2,3,16,17,18,19,32,33,34,35,48,49,50,51};
+
+            for (int i = 0; i < 16; i++)
+            {
+                pointsIndicesForPatches[3][i] = pointsIndicesForPatches[2][i] + 12;
+            }
+
+           
+            pointsIndicesForPatches[4] = new int[] { 0,4,8,12,16,20,24,28,32,36,40,44,48,52,56,60};
+
+            for (int i = 0; i < 16; i++)
+            {
+                pointsIndicesForPatches[5][i] = pointsIndicesForPatches[4][i] + 3;
+            }
+
+            List<Vector3[]> result = new List<Vector3[]>();
+            for(int i = 0; i < 6; i++)
+            {
+                Vector3[] patchPoints = new Vector3[16];
+                for(int j = 0; j < 16; j++)
+                {
+                    patchPoints[j] = points[pointsIndicesForPatches[i][j]].Position();
+                }
+
+                result.Add(patchPoints);
             }
 
             return result;
+        }
+
+        private Vector3[] GeneratePatchPoints(Vector3[] points)
+        {
+            Vector3[] allPatchPoints = new Vector3[(ConfigurationData.BezierBoxDivisions + 1) * (ConfigurationData.BezierBoxDivisions + 1)];
+            for(float u = 0; u <= 1; u += 1.0f / ConfigurationData.BezierBoxDivisions)
+            {
+                Vector3 four1 = new Vector3();
+                Vector3 four2 = new Vector3();
+                Vector3 four3 = new Vector3();
+                Vector3 four4 = new Vector3();
+
+                four1.X = HelpFunctions.DeKastilio(new float[] { points[0].X, points[1].X, points[2].X, points[3].X }, u, 4);
+                four1.Y = HelpFunctions.DeKastilio(new float[] { points[0].Y, points[1].Y, points[2].Y, points[3].Y }, u, 4);
+                four1.Z = HelpFunctions.DeKastilio(new float[] { points[0].Z, points[1].Z, points[2].Z, points[3].Z }, u, 4);
+
+                four2.X = HelpFunctions.DeKastilio(new float[] { points[4 + 0].X, points[4 + 1].X, points[4 + 2].X, points[4 + 3].X }, u, 4);
+                four2.Y = HelpFunctions.DeKastilio(new float[] { points[4 + 0].Y, points[4 + 1].Y, points[4 + 2].Y, points[4 + 3].Y }, u, 4);
+                four2.Z = HelpFunctions.DeKastilio(new float[] { points[4 + 0].Z, points[4 + 1].Z, points[4 + 2].Z, points[4 + 3].Z }, u, 4);
+
+                four3.X = HelpFunctions.DeKastilio(new float[] { points[8 + 0].X, points[8 + 1].X, points[8 + 2].X, points[8 + 3].X }, u, 4);
+                four3.Y = HelpFunctions.DeKastilio(new float[] { points[8 + 0].Y, points[8 + 1].Y, points[8 + 2].Y, points[8 + 3].Y }, u, 4);
+                four3.Z = HelpFunctions.DeKastilio(new float[] { points[8 + 0].Z, points[8 + 1].Z, points[8 + 2].Z, points[8 + 3].Z }, u, 4);
+
+                four4.X = HelpFunctions.DeKastilio(new float[] { points[12 + 0].X, points[12 + 1].X, points[12 + 2].X, points[12 + 3].X }, u, 4);
+                four4.Y = HelpFunctions.DeKastilio(new float[] { points[12 + 0].Y, points[12 + 1].Y, points[12 + 2].Y, points[12 + 3].Y }, u, 4);
+                four4.Z = HelpFunctions.DeKastilio(new float[] { points[12 + 0].Z, points[12 + 1].Z, points[12 + 2].Z, points[12 + 3].Z }, u, 4);
+
+                for (float v = 0; v <= 1; v += 1.0f / ConfigurationData.BezierBoxDivisions)
+                {
+                    Vector3 resultPoint = new Vector3();
+                    resultPoint.X = HelpFunctions.DeKastilio(new float[] { four1.X, four2.X, four3.X, four4.X }, v, 4);
+                    resultPoint.Y = HelpFunctions.DeKastilio(new float[] { four1.Y, four2.Y, four3.Y, four4.Y }, v, 4);
+                    resultPoint.Z = HelpFunctions.DeKastilio(new float[] { four1.Z, four2.Z, four3.Z, four4.Z }, v, 4);
+                    allPatchPoints[(int)(u * ConfigurationData.BezierBoxDivisions) + (int)(v * ConfigurationData.BezierBoxDivisions)] = resultPoint;
+                }
+            }
+
+            return allPatchPoints;
+        }
+
+        private uint[] GenerateIndices()
+        {
+            List<uint> uints = new List<uint>();
+            for(uint i=0;i< ConfigurationData.BezierBoxDivisions; i++)
+            {
+                for (uint j = 0; j < ConfigurationData.BezierBoxDivisions; j++)
+                {
+                    uint shift = j * 51;
+                    uints.Add(i + shift);
+                    uints.Add(i + 1 + shift);
+                    uints.Add(i + 51 + shift);
+
+                    uints.Add(i + shift);
+                    uints.Add(i + 1 + shift);
+                    uints.Add(i + 52 + shift);
+                }
+            }
+
+            return uints.ToArray();
         }
     }
 }
